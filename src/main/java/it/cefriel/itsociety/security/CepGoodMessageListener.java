ddf32a0056841ee0ddb72aeaf3cb6696a3f08466
Alessio Carenini <carenini@gmail.com>
2012-02-28 12:28:19 +0100

    Forward bad messages back to source
    If a component sends a message which is refused by user's ACL, the
    component receives back a message with a routing key =
    original_routing_key.refused

--- src/main/java/it/cefriel/itsociety/security/CepGoodMessageListener.java
+++ src/main/java/it/cefriel/itsociety/security/CepGoodMessageListener.java
@@ -1,10 +1,12 @@
 package it.cefriel.itsociety.security;
 
+import org.springframework.amqp.core.Message;
+
 import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UpdateListener;
 
 public class CepGoodMessageListener implements UpdateListener {
-
+	
     public CepGoodMessageListener() {
         
     }
@@ -12,6 +14,7 @@ public class CepGoodMessageListener implements UpdateListener {
 	@Override
 	public void update(EventBean[] newData, EventBean[] oldData) {
         System.out.println("Event received: "+ newData[0].getUnderlying());
+        Message good_message=(Message) newData[0].getUnderlying();
 	}
 }
 
--- src/main/java/it/cefriel/itsociety/security/RefusedMessageListener.java
+++ src/main/java/it/cefriel/itsociety/security/RefusedMessageListener.java
@@ -1,14 +1,40 @@
 package it.cefriel.itsociety.security;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+
 import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UnmatchedListener;
 
 public class RefusedMessageListener implements UnmatchedListener {
+	private AmqpTemplate amqp=null;
+	private String target_exchange=null;
+
+	public AmqpTemplate getAmqp() {
+		return amqp;
+	}
+
+	public void setAmqp(AmqpTemplate amqp) {
+		this.amqp = amqp;
+	}
+
+	public String getTarget_exchange() {
+		return target_exchange;
+	}
+
+	public void setTarget_exchange(String target_exchange) {
+		this.target_exchange = target_exchange;
+	}
 
 	@Override
 	public void update(EventBean arg0) {
+		String routing_key=null;
+		Message bad_message=null;
+		
         System.out.println("Message refused: "+ arg0.getUnderlying());
-
+        bad_message=(Message) arg0.getUnderlying();
+        routing_key=bad_message.getMessageProperties().getReceivedRoutingKey();
+        amqp.send(target_exchange,routing_key,bad_message);
 	}
 
 }
